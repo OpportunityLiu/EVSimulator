0432b4d3ffb255be8e83f58755581287624ce58f
OpportunityLiu <liuzhengyuan13@outlook.com>
2016-04-14 13:50:26 +0800

    Greenfoot hello world

---
+++ HomeWorld.java
@@ -0,0 +1,22 @@
+/**
+ * Write a description of class HomeWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import greenfoot.*;
+
+public class HomeWorld extends World
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class HomeWorld
+     */
+    public HomeWorld()
+    {
+        super(1024,768,1);
+        setBackground("a.bmp");
+    }
+}
